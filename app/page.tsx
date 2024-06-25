a1874cdd0a889207d498ee5bbe0f61889756768f
James Randolph <james.randolph2016@gmail.com>
2024-06-25 07:28:50 -0400

    add sketches

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 import { useForm } from "react-hook-form";
 import { useMutation } from "convex/react";
+import { ReactSketchCanvas, ReactSketchCanvasRef } from 'react-sketch-canvas';
+import { useRef } from "react";
+import { ReadableStreamDefaultController } from "stream/web";
 
 export default function Home() {
 
@@ -15,17 +18,30 @@ export default function Home() {
     prompt: string;
   }>()
 
+  const canvasRef = useRef<ReactSketchCanvasRef>(null);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form onSubmit={handleSubmit(async (formData) => {
-        const results = await saveSketchMutation(formData);
-        console.log(results);
+      <form 
+        className="flex flex-col gap-2" 
+        onSubmit={handleSubmit(async (formData) => {
+          if (!canvasRef.current) return;
+          const image = await canvasRef.current.exportImage("jpeg");
+          console.log("image: ", image);
+          const results = await saveSketchMutation({...formData, image });
+          // console.log(results);
       })}>
         <input {...register("prompt", { required: true })} />
         
         {errors.prompt && <span>This field is required</span>}
 
-        <input type="submit" />
+      <ReactSketchCanvas
+        ref={canvasRef}
+        style={{width:256, height:256 }}
+        strokeWidth={4}
+        strokeColor="black"
+      />
+        <input className="bg-blue-400 rounded text-white" type="submit" />
       </form>
     </main>
   );
--- convex/sketches.ts
+++ convex/sketches.ts
@@ -1,20 +1,69 @@
-import { mutation, query } from "./_generated/server"
+import { mutation, query, internalAction, internalMutation } from "./_generated/server";
+import Replicate from "replicate";
+import { string } from "prop-types";
+
+
 
 export const saveSketch = mutation(
-    async ({ db }, { prompt }: { prompt: string }) => {
+    async ({ db, scheduler }, { prompt, image }: { prompt: string, image: string }) => {
         console.log(prompt);
-        await db.insert("sketches", {
+        const sketch = await db.insert("sketches", {
             prompt,
-        })
+        });
         
+        await scheduler.runAfter(0, "sketches:generate", {
+            sketchId: sketch._id, 
+            prompt,
+            image,
+        });
+
         return {
-            message: "success"
+            sketch
         };
     }
 );
 
-export const getSketches = query( async ({ db }) => {
-        const sketches = await db.query("sketches").collect();
-        return sketches;
+export const generate = internalAction(
+    async (
+        {
+            runMutation
+        }, 
+        { 
+            prompt,
+            image,
+            sketchId
+        }: { sketchId: Id<string>; prompt: string, image: string }) => {
+
+    const replicate = new Replicate();
+    const input = {
+        image,
+        prompt,
+        scale: 7,
+        image_resolution: "512",
+        n_prompt: "longbody, lowres, bad anatomy, bad hands, missing fingers, extra fingers, cropped, low quality"
+    };
+    
+    const output = await replicate.run("jagilley/controlnet-scribble:435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117", { input });
+
+    await runMutation("sketches:updateSketchResult", {
+        sketchId,
+        result: output[1]
+    })
+    console.log(output)
+});
+
+export const updateSketchResult = internalMutation(
+    async (
+        { db },
+        { sketchId, result }: { sketchId: Id<string>; result: string }
+    ) => {
+        await db.patch(sketchId, {
+            result
+        });
     }
-)
+);
+
+export const getSketches = query(async ({ db }) => {
+    const sketches = await db.query("sketches").collect();
+    return sketches;
+});
