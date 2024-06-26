227355e56afb855918053f23ccdb2fa557cab9f3
James Randolph <james.randolph2016@gmail.com>
2024-06-26 15:23:01 -0400

    logging convex

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+console.log('convex',convex);
 
 export default function ConvexClientProvider({
   children,
